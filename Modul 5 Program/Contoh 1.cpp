7fd6b6adbbfed8f0f8dfc21e88103053b4c0bf2c
RiyanAstriWijaya <thekingofkelassore@gmail.com>
2024-06-05 07:34:06 +0700

    push

---
+++ Modul 5 Program/Contoh 1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int awal;
+	awal= 1;
+	while (awal <= 3){
+		awal= awal + 1;
+		cout <<"Siti Ririn Sutarsih"<<endl;
+	}
+}
---
+++ Modul 5 Program/Contoh 2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	int awal;
+	awal = 1;
+	do {
+		awal++;
+		cout<<"Nama Saya Siti Ririn Sutarsih"<<endl;
+	}
+	while (awal <= 3);
+}
